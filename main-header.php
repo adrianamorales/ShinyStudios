7845fb6ac24f7715d02f82613fc8c59b01cb3e5a
Adriana Morales <adriana.a.morales@live.com>
2013-11-05 22:18:56 -0500

    third commit

---
+++ main-header.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<title>Shiny Studios </title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Template based on Bootstrap">
+<meta name="author" content="Pixel-Coder">
+
+<!-- the styles -->
+<link href="css/supersized.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="theme/supersized.shutter.css" type="text/css" media="screen" />
+<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+<link href="css/colorbox.css" rel="stylesheet" media="screen">
+<link href="css/fonts.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="css/font-awesome.css" rel="stylesheet">
+<!--[if IE 7]>
+    <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css">
+    <![endif]-->
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="ico/favicon.png">
+</head>
+
+<body>
+<div class="spinner"></div>
+<!--+++++++++++- header+++++++++++-->
+<header>
+  <nav>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <a class="brand" href="index.html"><img  src="img/logo.png" alt="Pixopulus" class="logo"></a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li><a href="index.html"><span class="current"><i class="icon-home"></i></span> Home</a></li>
+              <li class="divider-vertical"></li>
+              <li><a href="portfolio_iso.html"><span class="current"><i class="icon-picture"></i></span> Portfolio</a>
+              <li class="divider-vertical"></li>
+              <li class="active"><a href="aboutus.html"><span class="current"><i class="icon-group"></i></span> About us</a></li>
+              <li class="divider-vertical"></li>
+              <li><a href="service.html"><span class="current"><i class="icon-check"></i></span> Service </a></li>
+              <li class="divider-vertical"></li>
+              <li><a href="contact.html"><span class="current"><i class="icon-envelope-alt"></i></span> Contact</a></li>
+              <li class="divider-vertical"></li>
+              <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Features <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="blog.html">Blog List</a></li>
+                  <li><a href="post-blog.html">Blog Post</a></li>
+                  <li><a href="full_width.html">Full Width Page</a></li>
+                  <li><a href="columns.html">Columns Example</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Extra</li>
+                  <li><a href="font_awesome.html">Font Awesome</a></li>
+                  <li><a href="list-styles.html">List Styles</a></li>
+                  <li><a href="box_tables.html">Box / Tables</a></li>
+                  <li><a href="404.html">404 Page</a></li>
+                  <li><a href="elements.html">Elements and Typo</a></li>
+                  <li class="dropdown-submenu">
+                    <a tabindex="-1" href="#">More options</a>
+                    <ul class="dropdown-menu">
+                      <li><a tabindex="-1" href="#">Second level link</a></li>
+                      <li><a tabindex="-1" href="#">Second level link</a></li>
+                      <li><a tabindex="-1" href="#">Second level link</a></li>
+                      <li><a tabindex="-1" href="#">Second level link</a></li>
+                      <li><a tabindex="-1" href="#">Second level link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+            </ul>
+          </div>
+          <!--/.nav-collapse --> 
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
+<!--+++++++++++- end header+++++++++++--> 
