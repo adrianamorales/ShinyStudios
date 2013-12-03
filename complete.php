3e69b04214bbe7c93367f9dbdffd602850bb4e21
Adriana <adriana.a.morales@live.com>
2013-12-03 00:56:43 -0500

    Template for Complete film

---
+++ complete.php
@@ -0,0 +1,216 @@
+
+<!--++ COMPLETE ++-->
+
+<?php
+	include('main-header.php');
+?>
+<!--+++++++++++- content++++++++++++++-->
+<div id="wrapper" >
+  <div id="page">
+    <div class="header">
+      <div class="container clearfix">
+        <div class="row">
+          <div class="span12"> 
+            <!-- Portfolio title -->
+            <h1>Film:<span class="raleway"> <span class="pink">//</span> Complete</span></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container contentStart">
+      <div class="row">
+        <div class="span12">
+         <div id="myCarousel" class="carousel slide proj-det"> 
+                  <!-- Carousel items -->
+                  <div class="carousel-inner main-img">
+                    <div class="active item"><a href="img/blog/post1.jpg" class="gallery_pics magnifier" title="Shadow in Motion" ><img alt="" src="img/blog/post1.jpg"></a></div>
+                    <div class="item"><a href="img/blog/post2.jpg" class="gallery_pics magnifier" title="Design with Passion" ><img alt="" src="img/blog/post2.jpg"></a></div>
+                  </div>
+                  <!-- Carousel nav --> 
+                  <a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a> <a class="carousel-control right" href="#myCarousel" data-slide="next">›</a> </div>
+        </div>
+      </div>
+      <div class="row contentStart">
+        <div class="span12">
+        <blockquote><p>"I used to get called faggot a lot... many different names. I would get bullied a lot on social networking. 
+        But I was just so used to my mom calling me names." - Nabeel</p></blockquote>
+        
+        <h1>The Story</h1>
+        <p>The news media loves reporting about school bullies and violent video games, as if we can neatly wrap the guilt package addressed to someone else. But what happens when parents are the abusers? What happens when teachers and counselors turn a blind eye? What happens when child protective services refuse to intervene? 19 year old Nabeel has finally escaped his mother's abuse, but over a decade of pain is hard to erase. </p>
+          
+        <blockquote><p>"When you're a child and you don't have good parents, you're helpless. You depend on adults and that's just something I never had." - Nabeel</p></blockquote>
+          
+        <p>A Bird Without Wings is a documentary that will focus on how the United States government handles child abuse by examining Nabeel's history with various child protective servies. Diving deep into Nabeel's past, we will see the physical, mental, and verbal abuse he suffered. Talking with experts in the field of law and social services, we will examine how Nabeel's case fell through the cracks. Following Nabeel, we will learn if it is possible for him to escape the terrifying memories in order to find his own identity.</p>
+
+				<h1>The Project</h1>
+       <p>A Bird Without Wings is a documentary that the director, Angela Wolf, has wanted to do for a long time. It is a film that she is particularly attached to because she was there during some of Nabeel's hardest times. She knows firsthand about Nabeel's physical and emotional abuse at the hands of his mother, and of the lack of action from his father to stop Nabeel's abuse. </p>
+        
+       <p>Nabeel was first sent to the hospital when he was 6 years old because of a beating his mother gave him. Unfortunately, however, she retained custody of Nabeel throughout his entire childhood. He wasn't able to be free of his mother's abuse until he became of legal age. </p>
+        
+       <p>Angela tried to help him when Nabeel told her about his situation, but soon found out that Child Protective Services couldn't do anything.  Being abused wasn’t enough for the judicial system; he had to show intense physical evidence of abuse before an investigation could be started. </p>
+        
+       <p>This is why Angela, the producers, and the rest of the crew want to make this film. It is time to tell this story, time to take action, time be a superhero and do something about child abuse.</p>
+          
+        </div>
+      </div>
+    </div>
+    
+
+  
+      <div class="gallerychooser">
+        <ul class="gallerychooserList">
+          <li class="current"><a data-filter="article.portfolio" href="#">All</a></li>
+          <li><a data-filter="article.portfolio[data-category~='design']" href="#">Design</a></li>
+          <li><a data-filter="article.portfolio[data-category~='photography']" href="#">Photography</a></li>
+          <li><a data-filter="article.portfolio[data-category~='branding']" href="#">Branding</a></li>
+          <li><a data-filter="article.portfolio[data-category~='video']" href="#">Video</a></li>
+        </ul>
+      </div>
+      <section class="portfolio_container">
+        <article class="portfolio" data-category="branding">
+          <section class="thumbImage"> <img src="img/portfolio/work1-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work1.jpg"  title="Give your pictures names."></a> 
+              </div>
+            </div>
+
+          </section>
+        </article>
+        
+        
+        <article class="portfolio" data-category="photography">
+          <section class="thumbImage"> <img src="img/portfolio/work2-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work2-thumb.jpg"  title="Give your pictures names."></a> 
+                </div>
+            		</div>
+          </section>
+        </article>
+        
+        
+        <article class="portfolio" data-category="video">
+          <section class="thumbImage"><img src="img/portfolio/work3-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work3.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        <article class="portfolio" data-category="design">
+          <section class="thumbImage"> <img src="img/portfolio/work4-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work4.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        
+        <article class="portfolio" data-category="photography">
+          <section class="thumbImage"> <img src="img/portfolio/work5-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work5.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        <article class="portfolio" data-category="video">
+          <section class="thumbImage"> <img src="img/portfolio/work6-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work6.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        <article class="portfolio" data-category="design">
+          <section class="thumbImage"> <img src="img/portfolio/work7-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work7.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        <article class="portfolio" data-category="photography">
+          <section class="thumbImage"> <img src="img/portfolio/work3-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work3.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        <article class="portfolio" data-category="video">
+          <section class="thumbImage"> <img src="img/portfolio/work2-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work2.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        <article class="portfolio" data-category="design">
+          <section class="thumbImage"> <img src="img/portfolio/work1-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work1.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+        
+        
+        <article class="portfolio" data-category="photography">
+          <section class="thumbImage"> <img src="img/portfolio/work2-thumb.jpg" alt="" class="fullwidth">
+            <div class="thumbTextWrap">
+              <div class="thumbText">
+                <h3 class="sectionTitle">Gallery Item</h3>
+                <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+                <a class="thumbLink portfolio_pics" href="img/portfolio/work2.jpg"  title="Give your pictures names."></a> 
+                </div>
+            </div>
+          </section>
+        </article>
+       
+    </div>
+
+    
+  </div>
+</div>
+<!--+++++++++++- end content++++++++++++++-->
+<?php
+	include('main-footer.php');
+?>
